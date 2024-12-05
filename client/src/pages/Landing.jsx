6d7474ee321a68c3ca547faf8eb49e62f909da88
Saket Kothari <sakkothari@deloitte.com>
2024-12-05 09:32:29 +0530

    styled components overview

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,4 +1,17 @@
+import styled from 'styled-components';
+
+const StyledBtn = styled.button`
+  font-size: 1.5rem;
+  background: red;
+  color: white;
+`;
+
 const Landing = () => {
-  return <div>Landing Page</div>;
-}
-export default Landing
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <StyledBtn>Styled Button</StyledBtn>
+    </div>
+  );
+};
+export default Landing;
