7f723dca2e818036bcaae96ac534f9b145a4513e
Saket Kothari <sakkothari@deloitte.com>
2024-12-05 10:23:25 +0530

    landing page implementation

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,25 +1,37 @@
-import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+import logo from '../assets/images/logo.svg';
+import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
 
 const Landing = () => {
   return (
     <Wrapper>
-      <h1>Landing Page</h1>
-      <div className="content">Some content</div>
+      <nav>
+        <img src={logo} alt="logo" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nam
+            vitae excepturi! Similique labore libero autem ea amet magnam ipsam
+            blanditiis molestias nihil, corrupti cupiditate? Beatae ab iure
+            quisquam totam.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job-hunt" className="img main-img" />
+      </div>
     </Wrapper>
   );
 };
-
-const Wrapper = styled.div`
-  background: red;
-
-  h1 {
-    color: white;
-  }
-
-  .content {
-    background: blue;
-    color: white;
-  }
-`;
 
 export default Landing;
