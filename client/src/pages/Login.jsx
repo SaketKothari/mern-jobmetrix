05551f4493fd7d32bfcb4f94f2700a1a62025716
Saket Kothari <sakkothari@deloitte.com>
2024-12-06 12:29:59 +0530

    login page setup

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,29 @@
 import { Link } from 'react-router-dom';
 
+import { FormRow, Logo } from '../components';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+
 const Login = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-      <Link to="/register">Register Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>login</h4>
+        <FormRow type="email" name="email" defaultValue="john@cena.com" />
+        <FormRow type="password" name="password" defaultValue="secret@123" />
+
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+        <button className="btn btn-block">explore the app</button>
+        <p>
+          Not a member yet?
+          <Link to="/register" className="member-btn">
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 export default Login;
