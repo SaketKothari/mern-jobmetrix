34b3348808a9a08967e2f5c7ff8b191b15fe8806
Saket Kothari <sakkothari@deloitte.com>
2024-12-06 12:13:49 +0530

    register page setup

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,19 +1,21 @@
 import { Link, useRouteError } from 'react-router-dom';
 
-import img from '../assets/images/not-found.svg'
+import img from '../assets/images/not-found.svg';
 import Wrapper from '../assets/wrappers/ErrorPage';
 
 const Error = () => {
   const error = useRouteError();
-  if(error.status === 404) {
-    return <Wrapper>
-      <div>
-        <img src={img} alt="not found" />
-        <h3>Page not found!</h3>
-        <p>We can't seem to find the page you're looking for</p>
-      <Link to="/dashboard">back home</Link>
-      </div>
-    </Wrapper>
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>Page not found!</h3>
+          <p>We can't seem to find the page you're looking for</p>
+          <Link to="/dashboard">back home</Link>
+        </div>
+      </Wrapper>
+    );
   }
 
   return (
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,39 @@
 import { Link } from 'react-router-dom';
 
+import Logo from '../components/Logo.jsx';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to="/login">Login Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="form-input"
+            defaultValue="john"
+            required
+          />
+        </div>
+
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+        <p>
+          Already a member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 export default Register;
