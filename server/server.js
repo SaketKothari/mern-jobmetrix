4c79967264fc91e346966bab2505c1f588a908d1
Saket Kothari <sakkothari@deloitte.com>
2024-12-17 21:46:12 +0530

    get all jobs route

--- server/server.js
+++ server/server.js
@@ -3,8 +3,14 @@ dotenv.config();
 
 import morgan from 'morgan';
 import express from 'express';
+import { nanoid } from 'nanoid';
 
 const app = express();
+
+let jobs = [
+  { id: nanoid(), company: 'apple', position: 'frontend' },
+  { id: nanoid(), company: 'google', position: 'backend' },
+];
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -19,6 +25,10 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   // console.log(req);
   res.json({ message: 'Data received', data: req.body });
+});
+
+app.get('/api/v1/jobs', (req, res) => {
+  res.status(200).json({ jobs });
 });
 
 const port = process.env.PORT || 5100;
