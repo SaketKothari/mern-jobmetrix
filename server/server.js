616348fefbed31499692c480207fb76ac7de2460
Saket Kothari <sakkothari@deloitte.com>
2024-12-17 10:13:59 +0530

    json middleware, morgan & dotenv setup

--- server/server.js
+++ server/server.js
@@ -1,10 +1,28 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import morgan from 'morgan';
 import express from 'express';
+
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello');
 });
 
-app.listen(5100, () => {
-  console.log(`Server is running...`);
+app.post('/', (req, res) => {
+  // console.log(req);
+  res.json({ message: 'Data received', data: req.body });
+});
+
+const port = process.env.PORT || 5100;
+
+app.listen(port, () => {
+  console.log(`Server running on PORT ${port}`);
 });
