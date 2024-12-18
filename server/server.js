d8ae9eeb972a26e9cba1a27b9bcca74777132232
Saket Kothari <sakkothari@deloitte.com>
2024-12-18 14:20:54 +0530

    edit & delete job functionality

--- server/server.js
+++ server/server.js
@@ -58,6 +58,39 @@ app.get('/api/v1/jobs/:id', (req, res) => {
   res.status(200).json({ job });
 });
 
+// EDIT JOB
+app.patch('/api/v1/jobs/:id', (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res
+      .status(400)
+      .json({ msg: 'Please provide company and position!' });
+  }
+
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `No job with id ${id}` });
+  }
+
+  job.company = company;
+  job.position = position;
+  res.status(200).json({ msg: 'Job modified successfully', job });
+});
+
+// DELETE JOB
+app.delete('/api/v1/jobs/:id', (req, res) => {
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+  if (!job) {
+    return res.status(404).json({ msg: `No job with id ${id}` });
+  }
+  const newJobs = jobs.filter((job) => job.id !== id);
+  jobs = newJobs;
+
+  res.status(200).json({ msg: 'Job deleted' });
+});
+
 const port = process.env.PORT || 5100;
 
 app.listen(port, () => {
