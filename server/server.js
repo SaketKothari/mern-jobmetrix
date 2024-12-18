35fccc1dc2b3fe7e16b3ecffbc42fa9e6ddaf718
Saket Kothari <sakkothari@deloitte.com>
2024-12-18 13:46:47 +0530

    create job functionality

--- server/server.js
+++ server/server.js
@@ -27,8 +27,24 @@ app.post('/', (req, res) => {
   res.json({ message: 'Data received', data: req.body });
 });
 
+// GET ALL JOBS
 app.get('/api/v1/jobs', (req, res) => {
   res.status(200).json({ jobs });
+});
+
+// CREATE A JOB
+app.post('/api/v1/jobs', (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res
+      .status(400)
+      .json({ msg: 'Please provide company and position!' });
+  }
+
+  const id = nanoid(10);
+  const job = { id, company, position };
+  jobs.push(job);
+  res.status(200).json({ job });
 });
 
 const port = process.env.PORT || 5100;
