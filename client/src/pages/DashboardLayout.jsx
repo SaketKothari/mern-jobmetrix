4311b3bfedfb3194645c11737db03d74605611df
Saket Kothari <sakkothari@deloitte.com>
2025-01-13 20:51:17 +0530

    get current user and send it as context in outlet

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -2,21 +2,23 @@ import { createContext, useContext, useState } from 'react';
 import { Outlet, redirect, useLoaderData } from 'react-router-dom';
 
 import { checkDefaultTheme } from '../App';
+import customFetch from '../utils/customFetch';
 import Wrapper from '../assets/wrappers/Dashboard';
 import { BigSidebar, Navbar, SmallSidebar } from '../components';
 
 export const loader = async () => {
-  return 'Get the data before component renders';
+  try {
+    const { data } = await customFetch.get('/users/current-user');
+    return data;
+  } catch (error) {
+    return redirect('/');
+  }
 };
 
 const DashboardContext = createContext();
 
 const DashboardLayout = () => {
-  const data = useLoaderData();
-  console.log(data);
-
-  // temp
-  const user = { name: 'John' };
+  const { user } = useLoaderData();
 
   const [showSidebar, setShowSidebar] = useState(false);
   const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme());
@@ -54,7 +56,7 @@ const DashboardLayout = () => {
           <div>
             <Navbar />
             <div className="dashboard-page">
-              <Outlet />
+              <Outlet context={{ user }} />
             </div>
           </div>
         </main>
