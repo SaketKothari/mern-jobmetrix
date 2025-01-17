831c024d740a863b35194b42ba8d7aefede909b6
Saket Kothari <sakkothari@deloitte.com>
2025-01-17 23:13:56 +0530

    access route parameter in loader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,9 +18,11 @@ import {
 
 import { action as loginAction } from './pages/Login';
 import { action as addJobAction } from './pages/AddJob';
+import { action as editJobAction } from './pages/EditJob';
 import { action as registerAction } from './pages/Register';
 
 import { loader as allJobsLoader } from './pages/AllJobs';
+import { loader as editJobLoader } from './pages/EditJob';
 import { loader as dashboardLoader } from './pages/DashboardLayout';
 
 // this function will run when components loads
@@ -69,6 +71,8 @@ const router = createBrowserRouter([
           {
             path: 'edit-job/:id',
             element: <EditJob />,
+            loader: editJobLoader,
+            action: editJobAction,
           },
         ],
       },
--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,4 +1,21 @@
+import { toast } from 'react-toastify';
+import { Form, useNavigation, redirect, useLoaderData } from 'react-router-dom';
+
+import customFetch from '../utils/customFetch';
+import { FormRow, FormRowSelect } from '../components';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { JOB_STATUS, JOB_TYPE } from '../../../server/utils/constants';
+
+export const loader = async ({ params }) => {
+  console.log(params);
+  return null;
+};
+
+export const action = async () => {
+  return null;
+};
+
 const EditJob = () => {
   return <div>EditJob Page</div>;
-}
-export default EditJob
+};
+export default EditJob;
