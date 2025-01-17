330e1131f738d65438b6f610af8302bf1f8f0200
Saket Kothari <sakkothari@deloitte.com>
2025-01-17 23:45:27 +0530

    delete job functionality

--- client/src/pages/DeleteJob.jsx
+++ client/src/pages/DeleteJob.jsx
@@ -1,4 +1,17 @@
-const DeleteJob = () => {
-  return <div>DeleteJob Page</div>;
-}
-export default DeleteJob
+import { toast } from 'react-toastify';
+import { redirect } from 'react-router-dom';
+
+import customFetch from '../utils/customFetch';
+
+export const action = async ({ request, params }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    await customFetch.delete(`/jobs/${params.id}`, data);
+    toast.success('Job deleted successfully');
+  } catch (error) {
+    toast.error(error?.response?.data?.msg);
+  }
+  return redirect('/dashboard/all-jobs');
+};
