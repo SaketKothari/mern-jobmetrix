39159767cd16165e64013d750771a74e6157075e
Saket Kothari <sakkothari@deloitte.com>
2025-01-22 23:41:12 +0530

    test user - login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { toast } from 'react-toastify';
-import { Link, Form, redirect } from 'react-router-dom';
+import { Link, Form, redirect, useNavigate } from 'react-router-dom';
 
 import customFetch from '../utils/customFetch';
 import { FormRow, Logo, SubmitBtn } from '../components';
@@ -20,6 +20,22 @@ export const action = async ({ request }) => {
 };
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const loginDemoUser = async () => {
+    const data = {
+      email: 'test@test.com',
+      password: 'secret123',
+    };
+    try {
+      await customFetch.post('/auth/login', data);
+      toast.success('Take a test drive to test the application!');
+      navigate('/dashboard');
+    } catch (error) {
+      toast.error(error?.response?.data?.msg);
+    }
+  };
+
   return (
     <Wrapper>
       <Form method="post" className="form">
@@ -28,7 +44,9 @@ const Login = () => {
         <FormRow type="email" name="email" defaultValue="john@cena.com" />
         <FormRow type="password" name="password" defaultValue="secret@123" />
         <SubmitBtn />
-        <button className="btn btn-block">explore the app</button>
+        <button className="btn btn-block" onClick={loginDemoUser}>
+          explore the app
+        </button>
         <p>
           Not a member yet?
           <Link to="/register" className="member-btn">
