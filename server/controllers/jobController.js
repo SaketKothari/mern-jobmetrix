845502312db5e2ac9a7a4e9a474a51b8903eae4a
Saket Kothari <sakkothari@deloitte.com>
2025-02-11 16:56:05 +0530

    stats route setup

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -1,4 +1,7 @@
 import Job from '../models/JobModel.js';
+
+import day from 'dayjs';
+import mongoose from 'mongoose';
 import { StatusCodes } from 'http-status-codes';
 
 export const getAllJobs = async (req, res) => {
@@ -29,4 +32,28 @@ export const updateJob = async (req, res) => {
 export const deleteJob = async (req, res) => {
   const removedJob = await Job.findByIdAndDelete(req.params.id);
   res.status(StatusCodes.OK).json({ msg: 'Job deleted', job: removedJob });
+};
+
+export const showStats = async (req, res) => {
+  const defaultStats = {
+    pending: 22,
+    interview: 11,
+    declined: 4,
+  };
+
+  let monthlyApplications = [
+    {
+      date: 'Dec 24',
+      count: 12,
+    },
+    {
+      date: 'Jan 25',
+      count: 9,
+    },
+    {
+      date: 'Feb 25',
+      count: 3,
+    },
+  ];
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
