7142898eb83a1014773f981d6f9e10ec93e5684f
Saket Kothari <sakkothari@deloitte.com>
2025-02-17 10:20:41 +0530

    group by monthly applications

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -56,19 +56,37 @@ export const showStats = async (req, res) => {
     declined: stats.declined || 0,
   };
 
-  let monthlyApplications = [
+  let monthlyApplications = await Job.aggregate([
     {
-      date: 'Dec 24',
-      count: 12,
+      $match: { createdBy: new mongoose.Types.ObjectId(`${req.user.userId}`) },
     },
     {
-      date: 'Jan 25',
-      count: 9,
+      $group: {
+        _id: {
+          year: { $year: '$createdAt' },
+          month: { $month: '$createdAt' },
+        },
+        count: { $sum: 1 },
+      },
     },
-    {
-      date: 'Feb 25',
-      count: 3,
-    },
-  ];
+    { $sort: { '_id.year': -1, '_id.month': -1 } },
+    { $limit: 6 },
+  ]);
+
+  monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item;
+
+      const date = day()
+        .month(month - 1)
+        .year(year)
+        .format('MMM YY');
+      return { date, count };
+    })
+    .reverse();
+
   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
