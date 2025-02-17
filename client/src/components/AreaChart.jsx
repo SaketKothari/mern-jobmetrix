e4703c74f85c4193ed2a5e752557d0c61dc9a4bc
Saket Kothari <sakkothari@deloitte.com>
2025-02-17 10:58:26 +0530

    using recharts for displaying area & bar chart

--- client/src/components/AreaChart.jsx
+++ client/src/components/AreaChart.jsx
@@ -1,7 +1,25 @@
-const AreaChart = () => {
-  return (
-    <div>AreaChart</div>
-  )
-}
+import {
+  Area,
+  XAxis,
+  YAxis,
+  Tooltip,
+  AreaChart,
+  CartesianGrid,
+  ResponsiveContainer,
+} from 'recharts';
 
-export default AreaChart
+const AreaChartComponent = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <AreaChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Area type="monotone" dataKey="count" stroke="#2cb1bc" fill="#befafd" />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default AreaChartComponent;
--- client/src/components/BarChart.jsx
+++ client/src/components/BarChart.jsx
@@ -1,7 +1,25 @@
-const BarChart = () => {
-  return (
-    <div>BarChart</div>
-  )
-}
+import {
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  BarChart,
+  CartesianGrid,
+  ResponsiveContainer,
+} from 'recharts';
 
-export default BarChart
+const BarChartComponent = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3 " />
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Bar dataKey="count" fill="#2cb1bc" barSize={75} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default BarChartComponent;
