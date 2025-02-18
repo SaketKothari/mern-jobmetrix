cef20427379fcbaec90cfd3ef44801cc78099c98
Saket Kothari <sakkothari@deloitte.com>
2025-02-18 15:25:20 +0530

    loader and query params to hit the url

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -5,9 +5,15 @@ import { useContext, createContext } from 'react';
 import customFetch from '../utils/customFetch';
 import { JobsContainer, SearchContainer } from '../components';
 
-export const loader = async () => {
+export const loader = async ({ request }) => {
+  // console.log(request.url);
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+  // console.log('Params', params);
+
   try {
-    const { data } = await customFetch.get('/jobs');
+    const { data } = await customFetch.get('/jobs', { params });
     return { data };
   } catch (error) {
     toast.error(error?.response?.data?.msg);
