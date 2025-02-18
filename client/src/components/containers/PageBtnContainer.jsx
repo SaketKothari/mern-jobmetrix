7bbf304299ea3f4820a3d895cdb938ea999cd771
Saket Kothari <sakkothari@deloitte.com>
2025-02-18 17:40:07 +0530

    pagination logic easy approach

--- client/src/components/containers/PageBtnContainer.jsx
+++ client/src/components/containers/PageBtnContainer.jsx
@@ -14,9 +14,27 @@ const PageBtnContainer = () => {
     return index + 1;
   });
 
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const handlePageChange = (pageNumber) => {
+    const searchParams = new URLSearchParams(search);
+    searchParams.set('page', pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
+  };
+
   return (
     <Wrapper>
-      <button className="btn prev-btn">
+      <button
+        className="btn prev-btn"
+        onClick={() => {
+          let prevPage = currentPage - 1;
+          if (prevPage < 1) {
+            prevPage = numberOfPages;
+          }
+          handlePageChange(prevPage);
+        }}
+      >
         <HiChevronDoubleLeft /> prev
       </button>
       <div className="btn-container">
@@ -27,13 +45,23 @@ const PageBtnContainer = () => {
                 pageNumber === currentPage && 'active'
               }`}
               key={pageNumber}
+              onClick={() => handlePageChange(pageNumber)}
             >
               {pageNumber}
             </button>
           );
         })}
       </div>
-      <button className="btn next-btn">
+      <button
+        className="btn next-btn"
+        onClick={() => {
+          let nextPage = currentPage + 1;
+          if (nextPage > numberOfPages) {
+            nextPage = 1;
+          }
+          handlePageChange(nextPage);
+        }}
+      >
         next
         <HiChevronDoubleRight />
       </button>
