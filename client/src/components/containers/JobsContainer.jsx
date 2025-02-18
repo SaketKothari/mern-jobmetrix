dec59acefea890d6b1fa63879d32f92a565cbf43
Saket Kothari <sakkothari@deloitte.com>
2025-02-18 16:47:53 +0530

    pagination setup + render buttons

--- client/src/components/containers/JobsContainer.jsx
+++ client/src/components/containers/JobsContainer.jsx
@@ -1,10 +1,11 @@
 import Job from '../Job';
+import PageBtnContainer from './PageBtnContainer';
 import { useAllJobsContext } from '../../pages/AllJobs';
 import Wrapper from '../../assets/wrappers/JobsContainer';
 
 const JobsContainer = () => {
   const { data } = useAllJobsContext();
-  const { jobs } = data;
+  const { jobs, totalJobs, numberOfPages } = data;
 
   if (!jobs.length === 0) {
     return (
@@ -15,11 +16,15 @@ const JobsContainer = () => {
   }
   return (
     <Wrapper>
+      <h5>
+        {totalJobs} job{jobs.length > 1 && 's'} found
+      </h5>
       <div className="jobs">
         {jobs.map((job) => {
           return <Job key={job._id} {...job} />;
         })}
       </div>
+      {numberOfPages > 1 && <PageBtnContainer />}
     </Wrapper>
   );
 };
--- client/src/components/containers/PageBtnContainer.jsx
+++ client/src/components/containers/PageBtnContainer.jsx
@@ -1,7 +1,44 @@
-const PageBtnContainer = () => {
-  return (
-    <div>PageBtnContainer</div>
-  )
-}
+import { useLocation, Link, useNavigate } from 'react-router-dom';
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi';
 
-export default PageBtnContainer
+import { useAllJobsContext } from '../../pages/AllJobs';
+import Wrapper from '../../assets/wrappers/PageBtnContainer';
+
+const PageBtnContainer = () => {
+  const {
+    data: { numberOfPages, currentPage },
+  } = useAllJobsContext();
+
+  // _ means we're gonna access the undefined value which we don't need
+  const pages = Array.from({ length: numberOfPages }, (_, index) => {
+    return index + 1;
+  });
+
+  return (
+    <Wrapper>
+      <button className="btn prev-btn">
+        <HiChevronDoubleLeft /> prev
+      </button>
+      <div className="btn-container">
+        {pages.map((pageNumber) => {
+          return (
+            <button
+              className={`btn page-btn ${
+                pageNumber === currentPage && 'active'
+              }`}
+              key={pageNumber}
+            >
+              {pageNumber}
+            </button>
+          );
+        })}
+      </div>
+      <button className="btn next-btn">
+        next
+        <HiChevronDoubleRight />
+      </button>
+    </Wrapper>
+  );
+};
+
+export default PageBtnContainer;
