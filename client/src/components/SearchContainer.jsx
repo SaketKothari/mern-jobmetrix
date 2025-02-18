8b64c54d040c5b4e7bf0a48b86d58f709848e0ec
Saket Kothari <sakkothari@deloitte.com>
2025-02-18 15:43:32 +0530

    controlled inputs & persisting the value bw page refreshes

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -10,34 +10,53 @@ import {
 } from '../../../server/utils/constants';
 
 const SearchContainer = () => {
+  const { searchValues } = useAllJobsContext();
+  const { search, jobStatus, jobType, sort } = searchValues;
+
+  const submit = useSubmit();
+
   return (
     <Wrapper>
       <Form className="form">
         <h5 className="form-title">search form</h5>
         <div className="form-center">
-          <FormRow type="search" name="search" defaultValue="a" />
+          <FormRow
+            type="search"
+            name="search"
+            defaultValue={search}
+            onChange={(e) => {
+              submit(e.currentTarget.form);
+            }}
+          />
           <FormRowSelect
             labelText="job status"
             name="jobStatus"
             list={['all', ...Object.values(JOB_STATUS)]}
-            defaultValue="all"
+            defaultValue={jobStatus}
+            onChange={(e) => {
+              submit(e.currentTarget.form);
+            }}
           />
           <FormRowSelect
             labelText="job type"
             name="jobType"
             list={['all', ...Object.values(JOB_TYPE)]}
-            defaultValue="all"
+            defaultValue={jobType}
+            onChange={(e) => {
+              submit(e.currentTarget.form);
+            }}
           />
           <FormRowSelect
             name="sort"
-            defaultValue="newest"
+            defaultValue={sort}
             list={[...Object.values(JOB_SORT_BY)]}
+            onChange={(e) => {
+              submit(e.currentTarget.form);
+            }}
           />
           <Link to="/dashboard/all-jobs" className="btn form-btn delete-btn">
             Reset Search Values
           </Link>
-          {/* TODO: Logic for submit */}
-          <SubmitBtn formBtn />
         </div>
       </Form>
     </Wrapper>
