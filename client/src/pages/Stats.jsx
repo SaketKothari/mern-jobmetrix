20015b4e4822aa7b122d4f140f3110b9381585f9
Saket Kothari <sakkothari@deloitte.com>
2025-02-19 11:32:25 +0530

    first query with react query to cache network call for stats

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,15 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
 import { useLoaderData } from 'react-router-dom';
 
 import customFetch from '../utils/customFetch';
 import { ChartsContainer, StatsContainer } from '../components';
 
 export const loader = async () => {
+  return null;
   const response = await customFetch.get('/jobs/stats');
   return response.data;
 };
 
 const Stats = () => {
-  const { defaultStats, monthlyApplications } = useLoaderData();
+  // const { defaultStats, monthlyApplications } = useLoaderData();
+
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ['stats'],
+    queryFn: () => customFetch.get('/jobs/stats'),
+  });
+
+  if (isLoading) return <h4>Loading...</h4>;
+  if (isError) return <h4>There was an error...</h4>;
+  const { defaultStats, monthlyApplications } = data.data;
 
   return (
     <>
