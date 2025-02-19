196c26cbc7d950305d7163859bf6c6d8ff179eb5
Saket Kothari <sakkothari@deloitte.com>
2025-02-19 12:38:31 +0530

    edit job react query

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,4 +1,5 @@
 import { toast } from 'react-toastify';
+import { useQuery } from '@tanstack/react-query';
 import { Form, redirect, useLoaderData } from 'react-router-dom';
 
 import customFetch from '../utils/customFetch';
@@ -6,33 +7,50 @@ import Wrapper from '../assets/wrappers/DashboardFormPage';
 import { FormRow, FormRowSelect, SubmitBtn } from '../components';
 import { JOB_STATUS, JOB_TYPE } from '../../../server/utils/constants';
 
-export const loader = async ({ params }) => {
-  try {
-    const { data } = await customFetch.get(`/jobs/${params.id}`);
-    return data;
-  } catch (error) {
-    toast.error(error?.response?.data?.msg);
-    return redirect('/dashboard/all-jobs');
-  }
+const singleJobQuery = (id) => {
+  return {
+    queryKey: ['job', id],
+    queryFn: async () => {
+      const { data } = await customFetch.get(`/jobs/${id}`);
+      return data;
+    },
+  };
 };
 
-export const action = async ({ request, params }) => {
-  const formData = await request.formData();
-  const data = Object.fromEntries(formData);
+export const loader =
+  (queryClient) =>
+  async ({ params }) => {
+    try {
+      await queryClient.ensureQueryData(singleJobQuery(params.id));
+      return params.id;
+    } catch (error) {
+      toast.error(error?.response?.data?.msg);
+      return redirect('/dashboard/all-jobs');
+    }
+  };
 
-  try {
-    await customFetch.patch(`/jobs/${params.id}`, data);
-    queryClient.invalidateQueries(['jobs']);
-    toast.success('Job edited successfully');
-    return redirect('/dashboard/all-jobs');
-  } catch (error) {
-    toast.error(error?.response?.data?.msg);
-    return error;
-  }
-};
+export const action =
+  (queryClient) =>
+  async ({ request, params }) => {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
 
-const EditJob = (queryClient) => () => {
-  const { job } = useLoaderData();
+    try {
+      await customFetch.patch(`/jobs/${params.id}`, data);
+      queryClient.invalidateQueries(['jobs']);
+      toast.success('Job edited successfully');
+      return redirect('/dashboard/all-jobs');
+    } catch (error) {
+      toast.error(error?.response?.data?.msg);
+      return error;
+    }
+  };
+
+const EditJob = () => {
+  const id = useLoaderData();
+  const {
+    data: { job },
+  } = useQuery(singleJobQuery(id));
 
   return (
     <Wrapper>
