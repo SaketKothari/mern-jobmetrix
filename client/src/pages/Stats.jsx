c601f0e311f5f6bb9ea6565981b7fc6923c54d28
Saket Kothari <sakkothari@deloitte.com>
2025-02-19 11:45:35 +0530

    using react query in stats loader

--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,26 +1,24 @@
 import { useQuery } from '@tanstack/react-query';
-import { useLoaderData } from 'react-router-dom';
 
 import customFetch from '../utils/customFetch';
 import { ChartsContainer, StatsContainer } from '../components';
 
-export const loader = async () => {
+const statsQuery = {
+  queryKey: ['stats'],
+  queryFn: async () => {
+    const response = await customFetch.get('/jobs/stats');
+    return response.data;
+  },
+};
+
+export const loader = (queryClient) => async () => {
+  const data = await queryClient.ensureQueryData(statsQuery);
   return null;
-  const response = await customFetch.get('/jobs/stats');
-  return response.data;
 };
 
 const Stats = () => {
-  // const { defaultStats, monthlyApplications } = useLoaderData();
-
-  const { isLoading, isError, data } = useQuery({
-    queryKey: ['stats'],
-    queryFn: () => customFetch.get('/jobs/stats'),
-  });
-
-  if (isLoading) return <h4>Loading...</h4>;
-  if (isError) return <h4>There was an error...</h4>;
-  const { defaultStats, monthlyApplications } = data.data;
+  const { data } = useQuery(statsQuery);
+  const { defaultStats, monthlyApplications } = data;
 
   return (
     <>
