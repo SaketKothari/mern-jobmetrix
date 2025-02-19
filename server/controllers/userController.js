92085b0854be1872aeae772bcd697f1c7e12f313
Saket Kothari <sakkothari@deloitte.com>
2025-02-19 10:21:19 +0530

    fix update user to use memory storage

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,9 @@
 import cloudinary from 'cloudinary';
-import { promises as fs } from 'fs';
 import { StatusCodes } from 'http-status-codes';
 
 import Job from '../models/JobModel.js';
 import User from '../models/UserModel.js';
+import { formatImage } from '../middleware/multerMiddleware.js';
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId });
@@ -20,9 +20,11 @@ export const getApplicationStats = async (req, res) => {
 export const updateUser = async (req, res) => {
   const newUser = { ...req.body };
   delete newUser.password;
+
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path);
+    const file = formatImage(req.file);
+
+    const response = await cloudinary.v2.uploader.upload(file);
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
