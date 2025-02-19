de5759f30b088b81442fc58cc6e8d05a49e246f4
Saket Kothari <sakkothari@deloitte.com>
2025-02-19 11:03:38 +0530

    react query setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import './assets/css/index.css';
 import {
@@ -37,6 +39,14 @@ export const checkDefaultTheme = () => {
 };
 
 checkDefaultTheme();
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
 
 const router = createBrowserRouter([
   {
@@ -89,6 +99,12 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-  return <RouterProvider router={router}></RouterProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
+
 export default App;
